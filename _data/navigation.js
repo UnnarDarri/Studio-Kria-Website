33171d237863ab99b5601ccb054e8850aec00e26
Reyn Alpha <reynalpha@outlook.com>
2026-08-02 21:38:01 +0000

    Add i18n

---
+++ _data/navigation.js
@@ -0,0 +1,27 @@
+export default [
+  {
+    "title": "front_page",
+    "url": "/",
+    "showInFooter": false,
+  },
+  {
+    "title": "projects",
+    "url": "/verkefni",
+    "showInFooter": true,
+  },
+  {
+    "title": "operations",
+    "url": "/starfsemi",
+    "showInFooter": true,
+  },
+  {
+    "title": "about",
+    "url": "/um-okkur",
+    "showInFooter": true,
+  },
+  {
+    "title": "contact",
+    "url": "/hafa-samband",
+    "showInFooter": true,
+  },
+];
--- eleventy.config.js
+++ eleventy.config.js
@@ -1,10 +1,82 @@
 import path from "node:path";
 import fs from "node:fs";
-import { HtmlBasePlugin } from "@11ty/eleventy";
+import { HtmlBasePlugin, I18nPlugin } from "@11ty/eleventy";
 import { eleventyImageTransformPlugin } from "@11ty/eleventy-img";
+import { fileURLToPath } from "node:url";
+import i18next from "i18next";
+import Backend from "i18next-fs-backend";
+import iso639 from "iso-639-1";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const defaultLanguage = "is";
 
 export default function (eleventyConfig) {
-  eleventyConfig.addPlugin(HtmlBasePlugin)
+  eleventyConfig.addPlugin(HtmlBasePlugin);
+
+  // i18n setup borrowed from https://blog.rubenwardy.com/2025/11/25/eleventy-translation/
+  eleventyConfig.addPlugin(I18nPlugin, {
+    defaultLanguage,
+    errorMode: "allow-fallback",
+  });
+
+  const locales = fs.readdirSync(path.join(__dirname, 'locales')).filter((fileName) => {
+    const joinedPath = path.join(path.join(__dirname, 'locales'), fileName)
+    const isDirectory = fs.lstatSync(joinedPath).isDirectory()
+    return isDirectory;
+  });
+
+  i18next.use(Backend).init({
+    initAsync: false,
+    lng: defaultLanguage,
+    saveMissing: true,
+    nsSeparator: false,
+    keySeparator: false,
+    fallbackLng: defaultLanguage,
+
+    backend: {
+      loadPath: path.join(__dirname, 'locales/{{lng}}/{{ns}}.json'),
+      addPath: path.join(__dirname, 'locales/{{lng}}/{{ns}}.json'),
+    },
+
+    preload: locales,
+  });
+
+  eleventyConfig.addFilter("i18n", function (msg, ...args) {
+    const t = i18next.getFixedT(this.page.lang ?? defaultLanguage);
+
+    if (args.length % 2 !== 0) {
+      throw new Error("i18n: expected even number of arguments");
+    }
+    const params = {};
+    for (let i = 0; i < args.length; i++) {
+      const key = args[i];
+      const value = args[i + 1];
+      params[key] = value;
+    }
+
+    return t(msg.replaceAll("[[", "{{").replaceAll("]]", "}}"), params);
+  });
+
+  eleventyConfig.addFilter("langName", (langCode) => {
+    return iso639.getNativeName(langCode.split("-")[0]);
+  });
+
+  // Generate each locale's layout files as needed
+  eleventyConfig.on("eleventy.before", () => {
+    fs.readdirSync(path.join(__dirname, "_includes/pages/")).forEach((f) => {
+      locales.forEach((locale) => {
+        const dest_filename = path.join(__dirname, locale, f);
+        if (!fs.existsSync(dest_filename)) {
+          fs.writeFileSync(dest_filename, `---\nlayout: pages/${f}\nlang: ${locale}\n---\n`);
+        }
+      });
+    });
+  });
+
+  // Using gitignore breaks building the site due to it ignoring the generated locale-specific layouts
+  eleventyConfig.setUseGitIgnore(false);
+
   eleventyConfig.addPlugin(eleventyImageTransformPlugin, {
     outputDir: ".cache/@11ty/img/",
     urlPath: "/img/built",
@@ -38,8 +110,13 @@ export default function (eleventyConfig) {
   
   eleventyConfig.on("eleventy.after", () => {
     fs.cpSync(".cache/@11ty/img/", path.join(eleventyConfig.directories.output, "/img/built/"), {
-      recursive: true
+      recursive: true,
     });
+  });
+
+  eleventyConfig.on("eleventy.after", () => {
+    // Copy the default language's index.html to root so that the bare site URL works
+    fs.cpSync(path.join(eleventyConfig.directories.output, defaultLanguage, "/index.html"), path.join(eleventyConfig.directories.output, "/index.html"));
   });
 
   eleventyConfig.addPassthroughCopy("static");
