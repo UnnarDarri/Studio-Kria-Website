733db46fe4efb65c003f8b36356cfc3b63ac05fb
Reyn Alpha <reynalpha@outlook.com>
2026-08-02 16:48:00 +0000

    Add 11ty, remove redundant code

---
+++ eleventy.config.js
@@ -0,0 +1,52 @@
+import path from "node:path";
+import fs from "node:fs";
+import { HtmlBasePlugin } from "@11ty/eleventy";
+import { eleventyImageTransformPlugin } from "@11ty/eleventy-img";
+
+export default function (eleventyConfig) {
+  eleventyConfig.addPlugin(HtmlBasePlugin)
+  eleventyConfig.addPlugin(eleventyImageTransformPlugin, {
+    outputDir: ".cache/@11ty/img/",
+    urlPath: "/img/built",
+    formats: ["avif", "webp", "jpeg", "svg"],
+    svgShortCircuit: true,
+    transformOnRequest: false,
+    
+    // Maximum image widths
+    widths: [4096, 2048],
+
+    htmlOptions: {
+      imgAttributes: {
+        alt: "",
+        loading: "lazy",
+				decoding: "async",
+      }
+    },
+
+    sharpJpegOptions: {
+      quality: 80,
+    },
+
+    sharpWebpOptions: {
+      quality: 90,
+    },
+
+    sharpAvifOptions: {
+      quality: 70,
+    },
+  });
+  
+  eleventyConfig.on("eleventy.after", () => {
+    fs.cpSync(".cache/@11ty/img/", path.join(eleventyConfig.directories.output, "/img/built/"), {
+      recursive: true
+    });
+  });
+
+  eleventyConfig.addPassthroughCopy("static");
+  eleventyConfig.addPassthroughCopy("css");
+  eleventyConfig.addPassthroughCopy("js");
+}
+
+export const config = {
+  pathPrefix: "",
+}
